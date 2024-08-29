a40c544f703abcd775bd593447d62b9c5c56cc1d
filin <georgynt@gmail.com>
2024-08-30 00:14:32 +0500

    sign_data must return bytes

--- logic.py
+++ logic.py
@@ -95,8 +95,11 @@ if sys.platform == 'win32':
             signer.KeyPin = key_pin
             sd = win32.Dispatch(SIGNED_DATA)
             sd.Content = data
-            return sd.SignCades(signer, CADES_BES,
+            sign = sd.SignCades(signer, CADES_BES,
                                 detached_sign, CAPICOM_ENCODE_BASE64)
+            if isinstance(sign, str):
+                sign = sign.encode()
+            return sign
 
 
 class MockCert:
